38a4ac9d54691051cbc5ea8485a494005011bc03
Benny Mirisola <bennymirisola@gmail.com>
2019-12-06 00:21:45 -0500

    Readded comments

--- Project_Drive_To_Wall/Project_Drive_To_Wall.ino
+++ Project_Drive_To_Wall/Project_Drive_To_Wall.ino
@@ -1,3 +1,4 @@
+//Wemos libraries
 #include <WemosInit.h>
 #include <Servo.h>
 
@@ -9,15 +10,9 @@ Servo motor2;
 
 long duration_C;
 
-const int THRESHOLD1 = 20;
-const int THRESHOLD2 = 10;
-const int THRESHOLD3 = 6;
-const int THRESHOLD4 = 5;
+const int THRESHOLD = 5;
 
 const int speed1 = 80;
-const int speed2 = 80;
-const int speed3 = 80;
-const int speed4 = 80;
 
 const int timeForSpin = 600;
 //700
@@ -30,21 +25,27 @@ boolean turned = false;
 boolean finished = false;
 
 
-void setup()
-{
-  Serial.begin(115200);
-  pinMode(D3,OUTPUT); 
-  pinMode(D5,INPUT);
+void setup(){
+  Serial.begin(115200); // Set baud rate to serial monitor
+  
+  pinMode(D3,OUTPUT); //sets trigger pin of ultrasonic to port D3
+  pinMode(D5,INPUT); //sets echo pin of ultrasonic to port D5
+
+  //Initializes left and right motors to their respective pins
   motor1.attach(motor1pin);
   motor2.attach(motor2pin);
-  delay(3000);
-
   
+  delay(3000); //Delay 3 seconds to allow for time to open Serial Monitor after program upload
 }
 
-void spin(int speed)
-
-{
+/* @function    spin   
+ * @discussion          Function to move the motors forwards and backwards. 
+ *                      Code automatically reverses delivered voltage to leftmotor
+ *                      because motors are mirror imaged.
+ * @param       speed   the speed the motors should be set to. 
+ *                      Closer to zero means forwards. 180 represents backwards. 90 = stop
+ */
+void spin(int speed){
 
 motor1.write(speed);
 if(speed < 90){
@@ -59,11 +60,20 @@ else if(speed > 90){
 
 }
 
+/* @function    stop   
+ * @discussion  Function to stop all motors. Motors set to 90 speed.
+ */
 void stop (){
 motor1.write(90);
 motor2.write(90);
 }
 
+/* @function    calculateDistance   
+ * @discussion  Function to calculate distance object is to front of ultrasonic sensor.
+ *              Iterates once every samplerate value in milliseconds
+ *              Obtains ultrasnic echo duration and returns this value divided by inchesDivisor factor
+ *              to return distance as a float in inches
+ */
 double calculateDistance(){
  delay(sampleRate);
  duration_C = ultrasonicPing(D3,D5);
@@ -71,25 +81,15 @@ double calculateDistance(){
  return distance_inches;  
 }
 
+/* @function    stop   
+ * @discussion  Function to drive robot to certain distance threshold from wall
+ *              drives forwards at speed until threshold constant is returned by calculateDistance.
+ *              Once distance is achieved, robot stops and drivenForwards class variable is set to true.
+ */
 void driveToWall(){
-  if(calculateDistance() >= THRESHOLD1){
+  if(calculateDistance() >= THRESHOLD){
     spin(speed1);
     //drivenForwards = false;
- }
-
- else if(calculateDistance() >= THRESHOLD2 && calculateDistance() < THRESHOLD1){
-  spin(speed2);
-  //drivenForwards = false;
- }
-
- else if(calculateDistance() >= THRESHOLD3 && calculateDistance() < THRESHOLD2){
-  spin(speed3);
-  //drivenForwards = false;
- }
-
- else if(calculateDistance() >= THRESHOLD4 && calculateDistance() < THRESHOLD3){
-  spin(speed4);
-  //drivenForwards = false;
  }
 
  else if (calculateDistance() < 4){
@@ -98,9 +98,14 @@ void driveToWall(){
    //Serial.println("Finished Baby!!!!!!");
  }
 
-  
 }
 
+/* @function    turn   
+ * @discussion          Function to turn the robot left for a predetermined 
+ *                      and claibrated amount of time. Robot moves at 70 speed.
+ *                      Then stops and sets turned class variable to true.
+ * @param timeForSpin   Duration in milliseconds for turning of robot. 
+ */
 void turn(){
   delay(1500);
   motor1.write(70);
@@ -116,21 +121,41 @@ void loop()
 {
   
    Serial.println(calculateDistance());
+   /*
+    * First checks if finished boolean is true
+    * If condition is met the rest of the if chain is not checked
+    * and robot stops moving
+    */
    if(finished){
     stop();
     Serial.println("Done");
   }
-  
+
+  /*
+   * Checks if drivenForwards variable is false
+   * If it is robot runs driveToWall method
+   * else robot moves onto Turn method call check
+   */
   else if(drivenForwards != true && !finished){
       driveToWall();
       Serial.println("forwards");
   }
   
+  /*
+   * Checks if drivenForwards variable is true and turned variable is false.
+   * If condition is met robot runs turn method
+   * else robot moves onto second driveToWall method call check
+   */
   else if(drivenForwards && !finished && turned != true){
       turn();
       Serial.println("Turning");
   }
-
+  /*
+   * Checks if turned variable is true.
+   * If condition is met robot runs driveToWall Method
+   * Distance is also checked and if it <=2 the finish class
+   * variable is set to true
+   */
   else if(turned && !finished){
      driveToWall();
      if(calculateDistance() <=2){
@@ -138,5 +163,4 @@ void loop()
      }
      Serial.println("Reverse");
   }
-  
 }
